afc6737a17447afb3f7a0eaaad85215fe7aef34f
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-06 13:10:21 -0200

    Students migrations

---
+++ database/migrations/2018_02_06_144747_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('cpf', 14)->unique();
+            $table->string('rg', 14);
+            $table->date('nascimento');
+            $table->string('nome');
+            $table->string('telefone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
