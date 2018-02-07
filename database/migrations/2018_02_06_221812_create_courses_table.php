735ae922dc4185e1f04cedc5edb023f48fcc5deb
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-06 22:22:43 -0200

    Migration cursos

---
+++ database/migrations/2018_02_06_221812_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->decimal('mensalidade', 8, 2)->unsigned();
+            $table->decimal('valor_matricula', 8, 2)->unsigned();
+            $table->string('periodo');
+            $table->integer('duracao')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
