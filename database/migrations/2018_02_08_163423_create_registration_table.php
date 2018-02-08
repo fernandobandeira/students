754c76bdab9d18480847766cb33642e9311109d4
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-08 16:45:11 -0200

    Migrations matriculas

---
+++ database/migrations/2018_02_08_163423_create_registration_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('ativa')->default(false);
+            $table->boolean('paga')->default(false);
+            $table->integer('ano')->unsigned();
+            $table->integer('student_id')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration');
+    }
+}
