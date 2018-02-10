6e9045119e009a9802adb63bdf354dd91277ff9b
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2018-02-10 00:31:24 -0200

    Migration dos pagamentos

---
+++ database/migrations/2018_02_10_002809_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('nome');
+            $table->decimal('valor', 8, 2)->unsigned();
+            $table->boolean('pago')->default(false);
+            $table->date('data_final');
+            $table->integer('registration_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('registration_id')->references('id')->on('registrations');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
